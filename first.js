edd42397bc61077ddf5064452c1fc6b577459af7
Love-kumar-sahu <64212145+Love-kumar-sahu@users.noreply.github.com>
2022-06-23 11:52:22 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+require('./startup/routes')(app);
+
+// process.on('uncaughtException',(ex) => {
+//     console.log('we got a error:',ex);
+// });
+
+mongoose.connect('mongodb://localhost/play')
+.then(()=>console.log('database connected....'))
+.catch( err => console.log('could not connected error',err));
+
+app.listen(3000, () => console.log('listeninn in port 3000'));
